851fd32dbc4cc42b0f9011bf2a8a032f215430fd
Noor <noormmlk@yahoo.com>
2020-11-28 18:32:27 -0700

    Edit auction
    edit the content of an auction

--- routing/routes.js
+++ routing/routes.js
@@ -1,26 +1,24 @@
-const express   = require('express');
-const routes    = express.Router();
-const User 		= require("../models/user");
+const express = require('express');
+const routes = express.Router();
+const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
-const {isLoggedIn} = require('../src/middleware');
-const Auction   = require("../models/auction");
+const { isLoggedIn } = require('../src/middleware');
+const Auction = require("../models/auction");
 const methodOverride = require("method-override");
-const mongoose      = require("mongoose");
+const mongoose = require("mongoose");
 const passport = require("passport");
 
 //Routing to all the pages.
 //Route to the landing page
-routes.get('/', (request, response)=>{
-
-    //Set up passport authentication
-         
-    response.redirect("/register"); 
+routes.get('/', (request, response) => {
+    //Set up passport authentication         
+    response.redirect("/register");
 })
 
 
 // start register 
-routes.get("/register" , (req,res) => {
-	res.render("users/register");
+routes.get("/register", (req, res) => {
+    res.render("users/register");
 
 });
 
@@ -31,7 +29,7 @@ routes.post('/register', catchAsync(async (req, res, next) => {
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
-           // req.flash('success', 'Welcome!');
+            // req.flash('success', 'Welcome!');
             res.redirect('/auctions');
         })
     } catch (e) {
@@ -43,10 +41,10 @@ routes.post('/register', catchAsync(async (req, res, next) => {
 // end register
 
 //Route to the login page
-routes.get('/login', (request, response)=>{
+routes.get('/login', (request, response) => {
     response.render('users/login')
 
-}) 
+})
 
 
 routes.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
@@ -55,85 +53,82 @@ routes.post('/login', passport.authenticate('local', { failureRedirect: '/login'
 });
 
 //Route to the logout page
-routes.get("/logout", (req,res) => {
-	req.logout();
-	//req.flash("success","Goodbye!");
-	res.redirect("/register");
-} );
+routes.get("/logout", (req, res) => {
+    req.logout();
+    //req.flash("success","Goodbye!");
+    res.redirect("/register");
+});
 
 //Route to the dashboard page
-routes.get('/auctions' ,async (request, response)=>{
+routes.get('/auctions', async (request, response) => {
     //If using mongoose, this issue can be solved by using
     //.lean() to get a json object (instead of a mongoose one):
-    const auctions = await Auction.find({}).lean();    
-    response.render('auctions/index', {auctions});
-}) 
+    const auctions = await Auction.find({}).lean();
+    response.render('auctions/index', { auctions });
+})
 
 //Route to the create an auction
-routes.get('/auctions/new' , async (request, response)=>{
+routes.get('/auctions/new', async (request, response) => {
     response.render('auctions/new');
-}) 
+})
 
 // create new auction form 
-routes.post("/auctions", async(req,res) => {
+routes.post("/auctions", async (req, res) => {
     const auction = new Auction(req.body.auction);
     await auction.save();
     res.redirect(`/auctions/${auction._id}`)
 });
 
-
-
 // not working 
 // edit auction in database and redirect to the same auction
-routes.put("/auctions/:id", async(req,res) => {
-    console.log("did it ")
-    const { id } = req.params;    
-    const auction = await Auction.findByIdAndUpdate(id,{...req.body.auction});
-    console.log("did it 3 ")
-    res.redirect(`/auctions/${auction}`)   
+routes.put("/auctions/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log((id));
+    console.log("one");
+    const auction = await Auction.findByIdAndUpdate(id, { ...req.body.auction });
+    console.log(auction);
+    res.redirect(`/auctions/${auction._id}`)
 });
 
 // delete
-routes.delete("/auctions/:id", async (req,res) =>{
-    const {id} = req.params;
+routes.delete("/auctions/:id", async (req, res) => {
+    const { id } = req.params;
     await Auction.findByIdAndDelete(id);
     res.redirect("/auctions");
 })
 
-
 // route to specific auction page
-routes.get('/auctions/:id' , async (request, response)=>{
-    const auction = await Auction.findById(request.params.id).lean(); 
-    
-    response.render('auctions/show', {id : auction._id , name : auction.name, price: auction.price, image: auction.image, description: auction.description});
-}) 
-
+routes.get('/auctions/:id', async (request, response) => {
+    const auction = await Auction.findById(request.params.id).lean();
+    response.render('auctions/show', { id: auction._id, name: auction.name, price: auction.price, image: auction.image, description: auction.description });
+})
 
 // route to edit specific auction page
-routes.get('/auctions/:id/edit' , async (request, response)=>{
-    const auction = await Auction.findById(request.params.id).lean();     
-    response.render('auctions/edit', {id : auction._id , name : auction.name, price: auction.price, image: auction.image, description: auction.description});
-}) 
+routes.get('/auctions/:id/edit', async (request, response) => {
+    //const auction = await Auction.findById(request.params.id).lean();
+    const auction = await Auction.findById(request.params.id);
+    response.render('auctions/edit', { id: auction._id, name: auction.name, price: auction.price, image: auction.image, description: auction.description });
+})
 
 
 
 //Route to the dashboard page
-routes.get('/dashboard', (request,response)=>{
+routes.get('/dashboard', (request, response) => {
     response.send("Hello, from the dashboard page");
 })
 
 
 //Route to the sell product page
-routes.get('/sell-product', (request, response)=>{
+routes.get('/sell-product', (request, response) => {
     response.send("Hello, from the sell product page");
 })
 
 
 //Route for the logout page- This redirects to the signin page
-routes.get('/logout', (request,response)=>{
+routes.get('/logout', (request, response) => {
     response.redirect('/signin');
 })
 
 
 //exporting the router module.
-module.exports= routes;
+module.exports = routes;
--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 
 //Install the Dependencies
-const express     	= require('express');
-const path        	= require('path');
-const app         	= express();
-const http        	= require('http').createServer(app);
-const session     	= require('express-session');
-const bodyParser  	= require('body-parser');
-const port        	= process.env.PORT || 2000; // used
-const router      	= require('../routing/routes');
-const hbs         	= require('express-handlebars');
-const mongoose      = require("mongoose");
-const passport    	= require("passport");
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').createServer(app);
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 2000; // used
+const router = require('../routing/routes');
+const hbs = require('express-handlebars');
+const mongoose = require("mongoose");
+const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User          = require("../models/user");
+const User = require("../models/user");
 const methodOverride = require("method-override");
 
 // mongo DB
 mongoose.connect('mongodb://localhost:27017/NASP_DB', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+    useFindAndModify: false
 
+});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -31,7 +32,7 @@ db.once("open", () => {
 
 
 // Static folder
-app.use(express.static(path.join(__dirname, 'src'), {extensions: ['html', 'htm']}) );
+app.use(express.static(path.join(__dirname, 'src'), { extensions: ['html', 'htm'] }));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
@@ -44,7 +45,7 @@ passport.deserializeUser(User.deserializeUser());
 // end passport 
 
 app.use((req, res, next) => {
-   // console.log(req.session)
+    // console.log(req.session)
     res.locals.currentUser = req.user;
     //res.locals.success = req.flash('success');
     //res.locals.error = req.flash('error');
@@ -53,13 +54,12 @@ app.use((req, res, next) => {
 
 //Setting up handlebars.
 app.engine('hbs', hbs({
-   extname: 'hbs',
-   defaultLayout:  path.join(__dirname, './views/main'),
-   partialsDir  : [
+    extname: 'hbs',
+    defaultLayout: path.join(__dirname, './views/main'),
+    partialsDir: [
         //  path to your partials
         path.join(__dirname, 'views/partials'),
-    ] 
-
+    ]
 }))
 
 //Setting up routing
@@ -67,5 +67,7 @@ app.use('/', router);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Start the server 
-http.listen(port, () => console.log(`Server is listening on port ${port}`));  
+// Start the server on port 2000
+http.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+});  
